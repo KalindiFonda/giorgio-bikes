e06a65183706231b230a89a2e82da03fe6b8f70b
Giorgio <giorgioce@outlook.com>
2020-10-12 16:23:46 +0100

    Release bike method & describe

--- Boris/Spec/docking_station_spec.rb
+++ Boris/Spec/docking_station_spec.rb
@@ -1,7 +1,11 @@
-require 'docking_station'
+require 'docking_station.rb'
 
 RSpec.describe DockingStation do
-
+  describe "#release_bike" do
+    it "Releases a bike" do
+      expect(subject.release_bike).to eq true
+    end
+  end
 
 
 end
