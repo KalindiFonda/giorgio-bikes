2d597d1d4630c744a3c3c2da59b02eda9b9b0183
Giorgio <giorgioce@outlook.com>
2020-10-13 16:49:28 +0100

    Q12 added guard condition

--- Boris/Lib/docking_station.rb
+++ Boris/Lib/docking_station.rb
@@ -1,8 +1,12 @@
 class DockingStation
   def release_bike
-    bike = Bike.new
+    # bike = Bike.new
+    fail "No bikes available" unless @bike
+    @bike
+    # raise
+    #'Argument is not numeric' unless x.is_a? Numeric
   end
-  attr_reader :bike
+  # attr_accessor :bike
   def dock_bike(bike)
     @bike = bike
   end
--- Boris/Spec/docking_station_spec.rb
+++ Boris/Spec/docking_station_spec.rb
@@ -3,14 +3,18 @@ require 'docking_station'
 RSpec.describe DockingStation do
   describe "#release_bike" do
     it "Releases a bike" do
-      expect(subject.release_bike).to be_instance_of Bike
+      expect{subject.release_bike}.to raise_error("No bikes available")
+      # giorgio_bike = Bike.new
+      # subject.dock_bike(giorgio_bike)
+      # expect(subject.release_bike).to be_instance_of Bike
+
     end
   end
-  describe "#release_bike.working?" do
-    it "Checks if release bike is working" do
-      expect(subject.release_bike.working?).to eq true
-    end
-  end
+  # describe "#release_bike.working?" do
+  #   it "Checks if release bike is working" do
+  #     expect(subject.release_bike.working?).to eq true
+  #   end
+  # end
   describe "#dock_bike" do
     it "Docking station accepts bike" do
       giorgio_bike = Bike.new
